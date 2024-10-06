a035a5d13f0f18bb8b56b5d9e1436635ba42b792
MingYeu <jasonlimmingyeu429@gmail.com>
2024-10-06 17:02:35 +0800

    connect with backend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Ubuntu } from "next/font/google";
+import { useTranslation, appWithTranslation } from "next-i18next";
 
-export default function App({ Component, pageProps }: AppProps) {
+const ubuntu = Ubuntu({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-ubuntu",
+});
+
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default appWithTranslation(App);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,43 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+// pages/index.tsx
+import { useEffect, useState } from "react";
 
-export default function Home() {
-  return <h1>Lim Ming Yeu</h1>;
+interface User {
+  id: number;
+  name: string;
+  email: string;
 }
+
+const Home = () => {
+  const [staffs, setStaffs] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/api/staff/staff"); // Changed to port 4000
+        const data = await response.json();
+        console.log("data", data);
+
+        setStaffs(data);
+      } catch (error) {
+        console.error("Error fetching staffs:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {staffs.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
