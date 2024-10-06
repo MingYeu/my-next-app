8734216e534cc212bce4243fee79b59c42cd0914
MingYeu <jasonlimmingyeu429@gmail.com>
2024-10-06 17:21:25 +0800

    change back the build function

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
-import withBundleAnalyzer from "@next/bundle-analyzer";
-
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-  ...nextConfig,
-});
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// import withBundleAnalyzer from '@next/bundle-analyzer';
+
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === 'true',
+//   ...nextConfig,
+// });
