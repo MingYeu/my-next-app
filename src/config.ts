09cf75ee982fb38177f79632ff87e7c251f53b67
MingYeu <jasonlimmingyeu429@gmail.com>
2024-10-13 05:53:00 +0800

    remove api domain

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? 'https://api.pioneer.wonsysmartech.com' : 'http://localhost:4000';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'http://localhost:4000' : 'http://localhost:4000'; //'https://api.pioneer.wonsysmartech.com' : 'http://localhost:4000';
 
 export const PLATFORM_NAME = '360 Toy Membership System';
 
